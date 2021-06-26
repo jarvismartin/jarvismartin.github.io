1db978f0a4a84cf6529604b78c0d307453df740b
j <jarvism@protonmail.com>
2021-06-26 13:58:44 -0400

    gh-pages commit

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ const ghpages = require("gh-pages");
 ghpages.publish(
   "public", // path to public directory
   {
-    branch: "master",
+    branch: "gh-pages",
     repo: "https://github.com/jarvismartin/jarvismartin.github.io.git", // Update to point to your repository
     user: {
       name: "Jarvis Martin", // update to use your name
